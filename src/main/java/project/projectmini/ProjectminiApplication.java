2e54ddaef45f1a160604884c6ec94190498a2e27
kims26 <2tkdgus3@gmail.com>
2024-02-25 17:16:28 +0900

    #1프로젝트환경설정

---
+++ src/main/java/project/projectmini/ProjectminiApplication.java
@@ -0,0 +1,13 @@
+package project.projectmini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectminiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectminiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/projectmini/ProjectminiApplicationTests.java
@@ -0,0 +1,13 @@
+package project.projectmini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectminiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
